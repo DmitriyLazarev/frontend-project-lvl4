76961f202e0eced459dfca2a318351168c527ad3
Dmitry Lazarev <dmitry.lazarev@team.wrike.com>
2022-07-13 15:58:34 +0200

    add rollbarConfig

--- src/init.jsx
+++ src/init.jsx
@@ -3,11 +3,21 @@ import { Provider as StoreProvider } from 'react-redux';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import filter from 'leo-profanity';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import store from './slices/index';
 import AuthProvider from './providers/authProvider';
 import App from './app';
 import ru from './locales/ru';
 import ChapProvider from './providers/charProvider';
+
+const rollbarConfig = {
+  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+  payload: {
+    environment: 'production',
+  },
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
 
 export default async () => {
   const i18n = i18next.createInstance();
@@ -24,13 +34,17 @@ export default async () => {
   filter.add(filter.getDictionary('en'));
   return (
     <StoreProvider store={store}>
-      <ChapProvider>
-        <I18nextProvider i18n={i18n}>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
-        </I18nextProvider>
-      </ChapProvider>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <ChapProvider>
+            <I18nextProvider i18n={i18n}>
+              <AuthProvider>
+                <App />
+              </AuthProvider>
+            </I18nextProvider>
+          </ChapProvider>
+        </ErrorBoundary>
+      </RollbarProvider>
     </StoreProvider>
   );
 };
