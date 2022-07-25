d8cf0ec5781e572f3223f69278250ea1924d8da7
Dmitry Lazarev <dmitry.lazarev@team.wrike.com>
2022-07-25 14:26:25 +0200

    fix overflow

--- application/src/components/chat.jsx
+++ application/src/components/chat.jsx
@@ -19,7 +19,7 @@ function Chat() {
 
   return (
     <Container
-      className="h-100 my-4 overflow-hidden rounded shadow"
+      className="h-100 my-4 rounded shadow"
     >
       <Row
         className="h-100 bg-white flex-md-row"
